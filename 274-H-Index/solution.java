a0920663e0ae854a0f2b072e63d15878dd8dffa0
Sting Chang <stingchang@users.noreply.github.com>
2016-07-30 15:03:42 -0700

    [274. H-Index][Compile Error]committed by LeetCode Extension

--- 274-H-Index/solution.java
+++ 274-H-Index/solution.java
@@ -1,15 +1,21 @@
 import java.util.Collections;
 public class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
+        
         //Arrays.sort(citations, Collections.reverseOrder());
         //[6,5,3,1,0]
         //[1,2,3,4,5]
-        int max = 5;
-        for(int i =len-1; i>=0; i--){
+        if(citations == null) return 0;
+        Arrays.sort(citations);
+        int len = citations.length;
+        
+        citation[0 1]
+        index   [0 1]
+        int max =0;
+        for(int i =0; i< len; i++){
             if(i+1<=citations[i])
-                return i+1;
+                max++;
         }
-        return 0;
+        return max;
     }
 }
