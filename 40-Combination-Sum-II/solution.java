0c3900e18c7a6df2048dde2bacabc9e0a6c70f21
Sting Chang <stingchang@users.noreply.github.com>
2016-08-10 16:58:45 -0700

    [40. Combination Sum II][Compile Error]committed by LeetCode Extension

---
+++ 40-Combination-Sum-II/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> lists = new ArrayList<>();
+        buildLists();
+    }
+    void buildLists(List<List<Integer>> lists , List<Integer> list , int index, int[] candidates, int target){
+        if(target < 0 || index<0){
+            return;
+        }
+        else if(target == 0){
+            lists.add(list);
+            return;
+        } else {
+            List<Integer> l1 = new ArrayList<>(list);
+            l1.add(candidates[index]);
+            buildLists(lists, l1, index-1, candidates, target-candidates[index]);
+            
+            List<Integer> l2 = new ArrayList<>(list);
+            buildLists(lists, l2, index-1, candidates, target);
+        }
+    }
+    
+}
