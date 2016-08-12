de468be0474fd3dc6a679f192f85a9f67764aa56
Sting Chang <stingchang@users.noreply.github.com>
2016-08-11 18:00:08 -0700

    [151. Reverse Words in a String][Accepted]committed by LeetCode Extension

--- 151-Reverse-Words-in-a-String/solution.java
+++ 151-Reverse-Words-in-a-String/solution.java
@@ -1,26 +1,33 @@
 public class Solution {
-    public String reverseWords(String s) {
-        s = s.trim();
+   public String reverseWords(String s) {
+		s = s.trim();
+//		System.out.println("s = "+s);
 		String[] arr = s.split("\\s+");
-		if(arr.length ==0)
-		    return "";
+		System.out.println(Arrays.toString(arr));
+		if (arr.length == 0)
+			return "";
 		StringBuilder sb = new StringBuilder();
-		for(int i =0; i< arr.length; i++){
+		for (int i = 0; i < arr.length; i++) {
+			
 			sb.append(reverse(arr[i]));
-			if(i<arr.length-1){
-			    sb.append(" ");    
+//			System.out.println("here");
+			if (i < arr.length - 1) {
+				sb.append(" ");
 			}
 		}
-
+//		System.out.println("sb = "+sb);
 		return sb.reverse().toString();
 	}
-	String reverse(String s){
+
+	String reverse(String s) {
 		char[] arr = s.toCharArray();
-		int a =0, b = arr.length-1;
-		while(a<b){
+		int a = 0, b = arr.length - 1;
+		while (a < b) {
 			char c = arr[a];
 			arr[a] = arr[b];
 			arr[b] = c;
+			a++;
+			b--;
 		}
 		return new String(arr);
 	}
