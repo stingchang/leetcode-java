b57a23de07abec95e1914c23d74b13ad01e15203
Sting Chang <stingchang@users.noreply.github.com>
2016-08-11 17:15:01 -0700

    [151. Reverse Words in a String][Compile Error]committed by LeetCode Extension

--- 151-Reverse-Words-in-a-String/solution.java
+++ 151-Reverse-Words-in-a-String/solution.java
@@ -1,33 +1,37 @@
 public class Solution {
-    public String reverseWords(String s) {
-        if(s.length() == 0)
+   s = s.trim();
+		System.out.println("\""+s+"\"");
+		int len = s.length() ;
+		
+		char[] arr = s.toCharArray();
+		int a = 0, b = len - 1;
+		// " xxxx xx x xx ", reverse sub array between front and end spaces
+		// " xx x xx xxxx "
+		while (a<len && arr[a] == ' ') {
+			a++;
+		}
+
+		while (b>=0 && arr[b] == ' ') {
+			b--;
+		}
+		if(a>=b)
 			return s;
-        char[] arr = s.toCharArray();
-        int a = 0, b = arr.length-1;
-        // "  xxxx xx x xx ", reverse sub array between front and end spaces
-        // "  xx x xx xxxx "
-        while(arr[a]==' '){
-            a++;
-        }
-        while(arr[b]==' '){
-            b--;
-        }
-        reverse(arr, a, b);
-        
-        // " 123 12 1 123" reverse each sub string
-        // " 321 21 1 321"
-        int c = a;
-        while(a<b){
-            while(c<b && arr[c]!=' '){
-                c++;
-            }
-            reverse(arr, a, c);
-            a = c+1;
-            while(a<b && arr[a]==' '){
-                a++;
-            }
-        }
-        return arr.toString();
+		reverse(arr, a, b);
+
+		// " 123 12 1 123" reverse each sub string
+		// " 321 21 1 321"
+		int c = a;
+		while (a < b) {
+			while (c < b && arr[c] != ' ') {
+				c++;
+			}
+			reverse(arr, a, c-1);
+			a = c + 1;
+			while (a < b && arr[a] == ' ') {
+				a++;
+			}
+		}
+		return new String(arr);
     }
     void reverse(char[] arr, int a, int b){
         while(a<b){
