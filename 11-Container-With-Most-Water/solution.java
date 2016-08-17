b85d1292a6bce71efadce7b0ea8c0db9c2d5918d
Sting Chang <stingchang@users.noreply.github.com>
2016-08-17 13:19:29 -0700

    [11. Container With Most Water][Compile Error]committed by LeetCode Extension

--- 11-Container-With-Most-Water/solution.java
+++ 11-Container-With-Most-Water/solution.java
@@ -4,20 +4,20 @@ public class Solution {
             return 0;
         
         int a = 0, b = height.length-1;
-        int max = Math.abs(height(a)-height(b)) * (b-a);
+        int max = Math.abs(height[a]-height[b]) * (b-a);
         while(b>a){
             
             int b2 = b;
-            while(b2>a && height(b2)<=height(b)){
+            while(b2>a && height[b2]<=height[b]){
                 b2--;
             }
-            int volR = Math.abs(height(a)-height(b2)) * (b2-a);
+            int volR = Math.abs(height[a]-height[b2]) * (b2-a);
             
             int b2 = a;
-            while(a2<b && height(a2)<=height(a)){
+            while(a2<b && height[a2]<=height[a]){
                 a2++;
             }
-            int volL = Math.abs(height(a2)-height(b)) * (b-a2);
+            int volL = Math.abs(height[a2]-height[b]) * (b-a2);
             
             if(volL>volR){
                 a = a2;
