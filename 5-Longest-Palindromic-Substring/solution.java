2a5a55c2a9e9fd4b5e76aa216729f5f9d9529c85
Sting Chang <stingchang@users.noreply.github.com>
2016-08-17 11:53:39 -0700

    [5. Longest Palindromic Substring][Runtime Error]committed by LeetCode Extension

--- 5-Longest-Palindromic-Substring/solution.java
+++ 5-Longest-Palindromic-Substring/solution.java
@@ -25,6 +25,8 @@ public class Solution {
         		maxMid =i;
         	}
         }
+        
+        System.out.println("maxWidth = "+maxWidth+", maxMid =" + maxMid+" "+Arrays.toString(c));
         // now we need to find out whether center character was inserted by us or not
         // -> characters at odd indices were inserted afterward
         // if maxMid is odd -> x.b.a.a.b.y
@@ -32,21 +34,33 @@ public class Solution {
         if(maxMid%2==1){
         	// x.b.a.a.b.y, mid = 5, maxWidth = 4(".b.a", "a.b.")
         	StringBuilder sb = new StringBuilder();
-        	for(int i =0; i*2< maxWidth/2; i++){
-        		sb.append(arr[maxMid+i*2+1]);
+        	for(int i =1; i<= maxWidth; i+=2){
+        		sb.append(c[maxMid+i]);
         	}
-        	sb = sb.reverse().append(sb);
-        	return sb.toString();
+//        	StringBuilder left = sb.reverse();
+//        	left.append(c[maxMid]).append(sb);
+        	
+        	String right = sb.toString();
+        	String left = sb.reverse().toString();
+        	String whole = left+c[maxMid]+right;
+//        	
+//        	System.out.println("case 1: "+whole);
+//        	
+        	
+        	return whole;
         } else {
         	// x.b.a.b.y, mid = 3, maxWidth = 4(".b.", ".b.")
         	StringBuilder sb = new StringBuilder();
-        	for(int i =2; i< maxWidth; i+=2){
-        		sb.append(arr[maxMid+i]);
+        	for(int i =2; i<= maxWidth; i+=2){
+        		sb.append(c[maxMid+i]);
         	}
-        	StringBuilder left = sb.reverse();
-        	left.append(arr[maxMid]);
-        	left.append(sb);
-        	return left.toString();
+        	String right = sb.toString();
+        	String left = sb.reverse().toString();
+        	String whole = left+c[maxMid]+right;
+        	
+//        	System.out.println("case 2: "+whole);
+//        	
+        	return whole;
         }
         
 
