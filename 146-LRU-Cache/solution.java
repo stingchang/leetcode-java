10dfd8f811ed30d7d953b1b279d114373ed57e73
Sting Chang <stingchang@users.noreply.github.com>
2016-08-20 15:33:16 -0700

    [146. LRU Cache][Compile Error]committed by LeetCode Extension

--- 146-LRU-Cache/solution.java
+++ 146-LRU-Cache/solution.java
@@ -1,23 +1,80 @@
 public class LRUCache {
-    
-	
-//	private int maxSize;
-	LinkedHashMap<Integer, Integer> map;
-	
-    public LRUCache(int capacity) {
-    	 map = new LinkedHashMap<>(capacity);
-//        maxSize = capacity;
-    }
-    
-    public int get(int key) {
-        int val = map.get(key);
-        return val;
-    }
-    
-    public void set(int key, int value) {
-        if(map.containsKey(key)){
-        	map.remove(key);
-        }
-        map.put(key, value);
-    }
+	private int maxSize;
+	private HashMap<Integer, Node> map;
+	private Node head, tail;
+
+	public Q146_LRUCache(int capacity) {
+		maxSize = capacity;
+		head = tail = null;
+	}
+
+	// return -1 if no such key
+	public int get(int key) {
+		if (!map.containsKey(key))
+			return -1;
+
+		int v = map.get(key).value;
+		remove(key);
+		set(key, v);
+		return v;
+	}
+
+	public void set(int key, int value) {
+		if (map.containsKey(key)) {
+			remove(key);
+		}
+		Node n = new Node(key, value);
+		n.post = head;
+		head.pre = n;
+		head = n;
+
+		if (map.size() > maxSize)
+			removeTail();
+
+	}
+
+	public void remove(int key) {
+		if (!map.containsKey(key)) {
+			return;
+		}
+
+		Node n = map.get(key);
+
+		if (n == head) {
+			removeHead();
+		}
+		if (n == tail) {
+			removeTail();
+		} else {
+			Node pre = n.pre;
+			Node post = n.post;
+			pre.post = post;
+			post.pre = pre;
+		}
+
+		map.remove(key);
+
+	}
+
+	public void removeHead() {
+		if (head == null)
+			return;
+		if (head == tail) {
+			head = tail = null;
+		} else {
+			head = head.post;
+			head.pre = null;
+		}
+	}
+
+	public void removeTail() {
+		if (tail == null)
+			return;
+		if (head == tail) {
+			head = tail = null;
+		} else {
+			tail = tail.pre;
+			tail.post = null;
+		}
+	}   
 }
