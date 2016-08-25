becb8ff3984e95960f12ef44841156d41e6e68a9
Sting Chang <stingchang@users.noreply.github.com>
2016-08-25 16:09:24 -0700

    [387. First Unique Character in a String][Compile Error]committed by LeetCode Extension

---
+++ 387-First-Unique-Character-in-a-String/solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int firstUniqChar(String s) {
+        // 1. use a hashmap and a queue to track frequency of each character
+        // 2. pop up characters from queue until current character appears only once
+        
+        Map<Character, Integer> map  = new HashMap<>();
+        Queue<Character, Integer> queue  = new LinkedList<>();
+        for(int i =0; i< s.length()-1; i++){
+            char c = s.charAt(i);
+            if(map.containsKey(c)){
+              map.put(c, map.get(c)+1);
+            } else {
+                map.put(c, 1);
+            }
+            queue.add(c);
+        }
+        int i = 0;
+        while(!queue.isEmpty()){
+            char c = queue.remove();
+            if(map.get(c) == 1)
+                return i;
+            i++;
+        }
+        
+        return -1;
+    }
+}
