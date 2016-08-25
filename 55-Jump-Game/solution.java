286d05e2c370400ef4e2ec5c541b1d21cbaba3a0
Sting Chang <stingchang@users.noreply.github.com>
2016-08-25 16:21:41 -0700

    [55. Jump Game][Runtime Error]committed by LeetCode Extension

--- 55-Jump-Game/solution.java
+++ 55-Jump-Game/solution.java
@@ -1,10 +1,11 @@
 public class Solution {
     public boolean canJump(int[] nums) {
-        int cur = nums[0];
+        int jump = nums[0];
         int i = 0;
-        while(cur!=0 && i<nums.length){
-            i = i+cur;
-            cur = nums[i];
+        while(jump!=0 && i<nums.length){
+            i = i+jump;
+            
+            jump = nums[i] < nums.length? nums[i]: 0 ;
         }   
         
         if(i == nums.length-1)
