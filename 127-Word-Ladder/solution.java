2c56a1606dd40691144a5d291ff705e21b99e5a1
Sting Chang <stingchang@users.noreply.github.com>
2016-08-28 09:57:56 -0700

    [127. Word Ladder][Accepted]committed by LeetCode Extension

--- 127-Word-Ladder/solution.java
+++ 127-Word-Ladder/solution.java
@@ -1,41 +1,60 @@
 public class Solution {
-  if(start==null || end==null || start.length()==0 || end.length()==0 || start.length()!=end.length())  
- 3         return 0; 
- 4         
- 5         LinkedList<String> wordQueue = new LinkedList<String>();
- 6         int level = 1; //the start string already count for 1
- 7         int curnum = 1;//the candidate num on current level
- 8         int nextnum = 0;//counter for next level
- 9         
-10         wordQueue.add(start);
-11         
-12         while(!wordQueue.isEmpty()){
-13             String word = wordQueue.poll();
-14             curnum--;
-15             
-16             for(int i = 0; i < word.length(); i++){
-17                 char[] wordunit = word.toCharArray();
-18                 for(char j = 'a'; j <= 'z'; j++){
-19                     wordunit[i] = j;
-20                     String temp = new String(wordunit);  
-21                     
-22                     if(temp.equals(end))
-23                         return level+1;
-24                     if(dict.contains(temp)){
-25                         wordQueue.add(temp);
-26                         nextnum++;
-27                         dict.remove(temp);
-28                     }
-29                 }
-30             }
-31             
-32             if(curnum == 0){
-33                 curnum = nextnum;
-34                 nextnum = 0;
-35                 level++;
-36             }
-37         }
-38         
-39         return 0;
+ 	public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+		if (beginWord == endWord)
+			return 1;
+		if(wordList.contains(beginWord))
+			wordList.remove(beginWord);
+		wordList.add(endWord);
+
+		Set<String> visiting = new HashSet<>();
+	
+		visiting.add(beginWord);
+
+		return dfs(visiting, wordList, endWord, 0);
+
+	}
+
+	int dfs(Set<String> visiting, Set<String> wordList, String end, int level) {
+		
+		
+		
+		while (!visiting.isEmpty()) {
+			level++;
+//		System.out.println(visiting.contains(end)+" "+visiting);
+			if(visiting.contains(end))
+				return level;
+			Set<String> nextlevel = new HashSet<>();
+			for (String s : visiting) {
+				char[] charArr = s.toCharArray();
+				for (int i = 0; i < s.length(); i++) {
+					char cur = charArr[i];
+					// for(int i =0; i< 26; i++){
+					for (char j = 'a'; j <= 'z'; j++) {
+//						char c = (char) ('a' + j);
+						
+						if (j != charArr[i]) {
+							char[] charArr2 = charArr.clone();
+							charArr2[i]=j;
+							String newStr = new String(charArr2);
+							if (newStr.equals( end))
+								return level+1;
+							// System.out.println("newStr = "+newStr);
+							if (wordList.contains(newStr)) {
+//								
+								nextlevel.add(newStr);
+								wordList.remove(newStr);
+							}
+						}
+					}
+
+				}
+			}
+			visiting = new HashSet<>();
+			visiting.addAll(nextlevel);
+//			nextlevel.clear();
+		}
+
+		return 0;
+	}
 
 }
